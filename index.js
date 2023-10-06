382648edfd28f551272e2edb597101e16431bdb5
Abarsanti22 <138410240+Abarsanti22@users.noreply.github.com>
2023-10-06 09:30:03 -0400

    added writetofile

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const { Circle, Square, Triangle } = require("./lib/shapes");
 
+function writeToFile(fileName, data) {
+  let svglogo = "";
+  svglogo =
+    '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+  svglogo += "<g>";
+  svglogo += `${data.shape}`;
