2f1e91a58be27c5d33f0c83d4a8ad15cfe025375
Abarsanti22 <138410240+Abarsanti22@users.noreply.github.com>
2023-10-06 09:30:38 -0400

    added shape choice

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ function writeToFile(fileName, data) {
     '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
   svglogo += "<g>";
   svglogo += `${data.shape}`;
+
+   let shapeChoice;
+  if (data.shape === "Circle") {
+    shapeChoice = new Circle();
+    svglogo += `<circle cx="150" cy="115" r="80" fill="${data.shapeBackgroundColor}"/>`;
+  } else if (data.shape === "Square") {
+    shapeChoice = new Square();
+    svglogo += `<rect x="70" y="40" width="150" height="150" fill="${data.shapeBackgroundColor}"/>`;
+  } else {
+    shapeChoice = new Triangle();
+    svglogo += `<polygon points="150, 20 245, 180 55, 180" fill="${data.shapeBackgroundColor}"/>`;
+  }
